ff453f7f087c06bf81f0eb012935e6cf138a4465
Chris Hunter <christopher.hunter@atos.net>
2024-10-21 10:58:49 +0100

    Delete UserConfig.java

--- backend/registration-service/src/main/java/com_example_registration_service/user/UserConfig.java
+++ backend/registration-service/src/main/java/com_example_registration_service/user/UserConfig.java
@@ -1,45 +1,45 @@
-package com_example_registration_service.user;
+// package com_example_registration_service.user;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Configuration;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.context.annotation.Configuration;
 
-@Configuration
-public class UserConfig {
+// @Configuration
+// public class UserConfig {
   
-  private final UserService userService;  // Injecting the UserService
+//   private final UserService userService;  // Injecting the UserService
 
-  @Autowired
-  public UserConfig(UserService userService) {
-    this.userService = userService;
-  }
+//   @Autowired
+//   public UserConfig(UserService userService) {
+//     this.userService = userService;
+//   }
   
-  // @Bean
-  // CommandLineRunner commandLineRunner(
-  //   UserRepository repository) {
-  //   return args -> {
-  //     // Hash the passwords using the hashPassword method from UserService
-  //     String hashedChrisPassword = userService.hashPassword("chrispassword");
-  //     String hashedMichaelPassword = userService.hashPassword("michaelpassword");
+//   // @Bean
+//   // CommandLineRunner commandLineRunner(
+//   //   UserRepository repository) {
+//   //   return args -> {
+//   //     // Hash the passwords using the hashPassword method from UserService
+//   //     String hashedChrisPassword = userService.hashPassword("chrispassword");
+//   //     String hashedMichaelPassword = userService.hashPassword("michaelpassword");
       
-  //     User chris = new User(
-  //       "Chris",
-  //       "Chris.Hunter@gmail.com",
-  //       LocalDate.of(1989,07,27),
-  //       hashedChrisPassword, 
-  //       "customer"
-  //     );
+//   //     User chris = new User(
+//   //       "Chris",
+//   //       "Chris.Hunter@gmail.com",
+//   //       LocalDate.of(1989,07,27),
+//   //       hashedChrisPassword, 
+//   //       "customer"
+//   //     );
       
-  //     User michael = new User(
-  //       "Michael",
-  //       "Michael.McCarthy@gmail.com",
-  //       LocalDate.of(1984,11,12),
-  //       hashedMichaelPassword,
-  //       "admin"
-  //     ); 
+//   //     User michael = new User(
+//   //       "Michael",
+//   //       "Michael.McCarthy@gmail.com",
+//   //       LocalDate.of(1984,11,12),
+//   //       hashedMichaelPassword,
+//   //       "admin"
+//   //     ); 
       
-  //     repository.saveAll(
-  //       List.of(chris, michael)
-  //     );
-  //   };
-  // }
-}
+//   //     repository.saveAll(
+//   //       List.of(chris, michael)
+//   //     );
+//   //   };
+//   // }
+// }
