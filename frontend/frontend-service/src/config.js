ccb0a51f18ac42f10d43c099096bef174b56f455
Christopher Hunter <christopher.hunter@digital.homeoffice.gov.uk>
2024-12-04 14:00:32 +0000

    Update API url

--- frontend/frontend-service/src/config.js
+++ frontend/frontend-service/src/config.js
@@ -1,7 +1,7 @@
 // File to contain any central config for importing 
 
 const config = {
-  API_URL: "http://registration-service:8080",
+  API_URL: "http://registration-service:80",
 };
 
 export default config;
