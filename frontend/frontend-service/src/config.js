a9ed4944d4a7c4bbd7798b8f65a247deaa9d6371
Christopher Hunter <christopher.hunter@digital.homeoffice.gov.uk>
2024-12-10 17:32:40 +0000

    Revert API_URL back to localhost

--- frontend/frontend-service/src/config.js
+++ frontend/frontend-service/src/config.js
@@ -1,7 +1,7 @@
 // File to contain any central config for importing 
 
 const config = {
-  API_URL: "http://registration-service",
+  API_URL: "http://localhost:8080",
 };
 
 export default config;
