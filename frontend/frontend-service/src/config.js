d4241a138c8aa3010172b42754566fe3644bc220
Christopher Hunter <christopher.hunter@digital.homeoffice.gov.uk>
2024-12-11 15:14:49 +0000

    Update dockerfiles to install curl
    Change API_URL to svc name
    
    Add port to API_URL
    
    Add FQDN to cors
    
    Allow ingress from nginx controller to backend
    
    Add ingress for registration-service
    
    Add backend domain to API_URL
    
    Add both custom domains to cors
    
    Pass only backend to cors policy
    
    Add both domains to cors
    
    Amend for one ingress only
    
    Update API_URL with FE domain
    
    Disable cors - test
    
    Amending frontend-service ingress

--- frontend/frontend-service/src/config.js
+++ frontend/frontend-service/src/config.js
@@ -1,7 +1,7 @@
 // File to contain any central config for importing 
 
 const config = {
-  API_URL: "http://localhost:8080",
+  API_URL: "http://frontend-service.dev.dsa-re-notprod.homeoffice.gov.uk",
 };
 
 export default config;
