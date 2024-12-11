c43b919ee263cbc59b7d236377fc607864e204c1
Christopher Hunter <christopher.hunter@digital.homeoffice.gov.uk>
2024-12-11 17:46:14 +0000

    Add port to API_URL

--- frontend/frontend-service/src/config.js
+++ frontend/frontend-service/src/config.js
@@ -1,7 +1,7 @@
 // File to contain any central config for importing 
 
 const config = {
-  API_URL: "http://registration-service",
+  API_URL: "http://registration-service:80",
 };
 
 export default config;
