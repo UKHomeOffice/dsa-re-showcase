3db1cf8c828095648ccd387ba9316a67f9119942
Christopher Hunter <christopher.hunter@digital.homeoffice.gov.uk>
2024-12-12 09:18:20 +0000

    Add both custom domains to cors

--- backend/registration-service/src/main/java/com_example_registration_service/WebConfig.java
+++ backend/registration-service/src/main/java/com_example_registration_service/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
   @Override
   public void addCorsMappings(CorsRegistry registry) {
       registry.addMapping("/**")
-              .allowedOrigins("http://frontend-service.dev.dsa-re-notprod.homeoffice.gov.uk") // Replace with your React app URL
+              .allowedOrigins("http://frontend-service.dev.dsa-re-notprod.homeoffice.gov.uk, http://registration-service.dev.dsa-re-dev.homeoffice.gov.uk") // Replace with your React app URL
               .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
               .allowedHeaders("*")
               .allowCredentials(true);
