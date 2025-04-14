e34e922a54d7879a7bf0cdc99cd5f7c1a5dc1533
Christopher Hunter <christopher.hunter@digital.homeoffice.gov.uk>
2025-04-14 11:58:35 +0100

    Created notification-service source code

---
+++ backend/notification-service/kafka_consumer.py
@@ -0,0 +1,73 @@
+import json
+import logging
+import asyncio
+from datetime import datetime
+from kafka import KafkaConsumer
+from ssl import create_default_context
+import os
+
+logger = logging.getLogger(__name__)
+    
+async def start_consumer(bootstrap_servers, topic, group_id, recent_logins, max_logins):
+  """Start consuming login events from kafka"""
+  logging.info(f"Starting Kafka consumer: {bootstrap_servers}, topic: {topic}, group: {group_id}")
+  
+  # Certificate info
+  cert_path = os.getenv('KAFKA_CERT_PATH', '/etc/kafka-certs')
+  cert_file = os.path.join(cert_path, 'leaf-cert.pem')
+  key_file = os.path.join(cert_path, 'leaf-key.pem')
+  ca_file = os.path.join(cert_path, 'root-cert.pem')
+  
+  logger.info(f"Using certificates at: {cert_path}")
+  
+  while True:
+    try:
+      # Create SSL context for mTLS
+      ssl_context = create_default_context()
+      ssl_context.load_cert_chain(cert_file, key_file)
+      ssl_context.load_verify_locations(ca_file)
+      
+      # Create a kafka consumer
+      consumer = KafkaConsumer(
+        topic,
+        bootstrap_servers=bootstrap_servers, 
+        group_id=group_id,
+        security_protocol='SSL',
+        ssl_context=ssl_context,
+        session_timeout_ms=30000,
+      )
+      
+      logger.info("Kafka consumer initialised, waiting for messages...")
+      
+      # Process messages
+      for message in consumer:
+        try:
+            raw = message.value.decode('utf-8').strip()
+            logging.info(f"Raw login message: {raw}")
+
+            # formatting:
+            parts = raw.split(", ")
+            if len(parts) == 3 and "User logged in: " in parts[0]:
+                name = parts[0].split("User logged in: ")[1].strip()
+                email = parts[1].strip()
+                timestamp = parts[2].strip()
+
+                logging.info(f"Login detected: {name} ({email}) at {timestamp}")
+                logging.info(f"Notification sent to {email}")
+
+                login_event = {
+                    "name": name,
+                    "email": email,
+                    "timestamp": timestamp
+                }
+                recent_logins.insert(0, login_event)
+                if len(recent_logins) > max_logins:
+                    recent_logins = recent_logins[:max_logins]
+            else:
+                logging.warning("Unexpected message format. Skipping.")
+
+        except Exception as e:
+            logging.error(f"Error processing message: {e}")
+    except Exception as e:
+      logger.error(f"Kafka connection error: {e}")
+      await asyncio.sleep(5)  
