ba8e378031f97a8a4f74def60ff7d5e0fb1ef975
Gador Cruz <168179692+gadorcruz@users.noreply.github.com>
2025-05-29 17:24:17 +0100

    endpoint OTLP exporter

--- backend/notification-service/custom_metric.py
+++ backend/notification-service/custom_metric.py
@@ -9,7 +9,7 @@ os.environ["REQUESTS_CA_BUNDLE"] = "/app/acp_root_ca.crt"
 
 # Initialize OpenTelemetry Metric Exporter
 exporter = OTLPMetricExporter(
-    endpoint=os.getenv("DYNATRACE_API_URL"),
+    endpoint="https://dynatrace-activegate-notprod.dynatrace.svc.cluster.local:4318/v1/metrics",
     headers={
         "Authorization": f"Api-Token {os.getenv('DYNATRACE_PAAS_TOKEN')}",
         "Content-Type": "application/x-protobuf"  
