b6b52104b912a533251bb974aada9de40bd6f3ae
Gador Cruz <168179692+gadorcruz@users.noreply.github.com>
2025-06-09 12:54:43 +0100

    fixing trace exporter

--- backend/notification-service/otel_config.py
+++ backend/notification-service/otel_config.py
@@ -11,7 +11,16 @@ from opentelemetry.sdk.trace.export import BatchSpanProcessor
 os.environ["REQUESTS_CA_BUNDLE"] = "/app/acp_root_ca.crt"
 
 # Initialize OpenTelemetry Metric Exporter
-exporter = OTLPMetricExporter(
+metric_exporter = OTLPMetricExporter(
+    endpoint="https://dynatrace-activegate-notprod.dynatrace.svc.cluster.local/e/ewo35763/api/v2/otlp/v1/metrics",
+    headers={
+        "Authorization": f"Api-Token {os.getenv('DYNATRACE_PAAS_TOKEN')}",
+        "Content-Type": "application/x-protobuf"
+    },
+)
+
+# Initialize OpenTelemetry Trace Exporter
+trace_exporter = OTLPSpanExporter(
     endpoint="https://dynatrace-activegate-notprod.dynatrace.svc.cluster.local/e/ewo35763/api/v2/otlp/v1/metrics",
     headers={
         "Authorization": f"Api-Token {os.getenv('DYNATRACE_PAAS_TOKEN')}",
@@ -20,13 +29,13 @@ exporter = OTLPMetricExporter(
 )
 
 # Set up the MeterProvider with the Metric Exporter
-metric_reader = PeriodicExportingMetricReader(exporter)
+metric_reader = PeriodicExportingMetricReader(metric_exporter)
 meter_provider = MeterProvider(metric_readers=[metric_reader])
 metrics.set_meter_provider(meter_provider)
 
 # Set up the TracerProvider with the Trace Exporter
 tracer_provider = TracerProvider()
-span_processor = BatchSpanProcessor(exporter)
+span_processor = BatchSpanProcessor(trace_exporter)
 tracer_provider.add_span_processor(span_processor)
 trace.set_tracer_provider(tracer_provider)
 
